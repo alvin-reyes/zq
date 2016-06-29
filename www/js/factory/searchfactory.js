9cda1189928dadd1d00af692e7c1fd657e43f725
Alvin Reyes <alvinpermelonareyes@gmail.com>
2016-06-28 23:35:19 -0400

    added search nearby places

--- www/js/factory/searchfactory.js
+++ www/js/factory/searchfactory.js
@@ -1,5 +1,5 @@
 angular.module('zatiqctrl.datafactory')
-.factory('nearbyfactory', function (gconfigfactory,$ionicLoading) {
+.factory('nearbyfactory', function (gconfigfactory,$cordovaGeolocation,$ionicLoading) {
         var result = [];
         var currentLat = 0;
         var currentLong = 0;
@@ -24,9 +24,9 @@ angular.module('zatiqctrl.datafactory')
                 var long = 0;
                 var currentLoc;
                 var map;
-
-                var currentLoc = navigator.geolocation.getCurrentPosition(function (position) {
-                    this.result = [];
+                var posOptions = {timeout: 10000, enableHighAccuracy: true};
+                var currentLoc = $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+                    
                     lat = position.coords.latitude;
                     long = position.coords.longitude;
                     currentLoc = new google.maps.LatLng(lat, long);
@@ -55,176 +55,13 @@ angular.module('zatiqctrl.datafactory')
                             console.log(result);
                         }
                     } 
+                },function(err) {
+                // error
                 });
                 return this;
             }
         };
     })
-.factory('foodfactory', function (gconfigfactory, $http, $ionicLoading) {
-        var result = [];
-        return {
-            result : function() {
-                return result;
-            },
-            getNearby: function(types,limit) {
-                var lat = 0;
-                var long = 0;
-                var currentLoc;
-                var map;
-
-                var currentLoc = navigator.geolocation.getCurrentPosition(function (position) {
-                    this.result = [];
-                    lat = position.coords.latitude;
-                    long = position.coords.longitude;
-                    currentLoc = new google.maps.LatLng(lat, long);
-
-                    map = new google.maps.Map(document.getElementById('map'), {
-                        center: currentLoc
-                        , zoom: 15
-                    });
-
-                    var request = {
-                        location: currentLoc
-                        , radius: '500'
-                        , types: types
-                    };
-
-                    $ionicLoading.show({
-                        template: 'Loading...'
-                    }).then(function () {
-                        console.log("The loading indicator is now displayed");
-                    });
-
-                    service = new google.maps.places.PlacesService(map);
-                    service.nearbySearch(request, callback);
-
-                    function callback(results, status) {
-                        if (status == google.maps.places.PlacesServiceStatus.OK) {
-                            for (var i = 0; i < limit; i++) {
-                                var place = results[i];
-                                result.push(place);
-                            }
-                            $ionicLoading.hide();
-                            console.log(result);
-                        }
-                    } 
-                });
-                return this;
-            }
-        };
-    })
-.factory('nightlifefactory', function (gconfigfactory, $http, $ionicLoading) {
-
-        var nl = [
-            {
-                id: 0
-                , type: 'nightlife'
-                , name: 'Nightlife 1'
-                , desc: 'This is a NL'
-                , img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1920px-Good_Food_Display_-_NCI_Visuals_Online.jpg'
-                , meta: []
-            }
-            , {
-                id: 1
-                , type: 'nightlife'
-                , name: 'Nightlife 2'
-                , desc: 'This is a NL'
-                , img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/1024px-Foods_%28cropped%29.jpg'
-                , meta: []
-            }
-    ];
-
-        return {
-
-            getNLs: function () {
-                return nl;
-            },
-
-            getNearByNightlifes: function () {
-                var lat = 0;
-                var long = 0;
-                var result = null;
-                navigator.geolocation.getCurrentPosition(function (position) {
-                    lat = position.coords.latitude;
-                    long = position.coords.longitude;
-
-                    $ionicLoading.show({
-                        template: 'Loading...'
-                    }).then(function () {
-                        console.log("The loading indicator is now displayed");
-                    });
-
-                    $http({
-                        url: gconfigfactory.getGooglePlacesUrl() + '?location=' + lat + ',' + long + '&radius=900&type=food&key=' + gconfigfactory.getGooglePlacesAPI()
-                        , dataType: 'JSONP'
-                        , method: 'GET'
-                    }).success(function (res) {
-                        result = res;
-                        $ionicLoading.hide();
-                    });
-                });
-                return result;
-            }
-        };
-    })
-.factory('placesfactory', function (gconfigfactory, $http, $ionicLoading) {
-    var places = [
-        {
-            id: 0
-            , type: 'nightlife'
-            , name: 'Places 1'
-            , desc: 'This is a Place'
-            , img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/1920px-Good_Food_Display_-_NCI_Visuals_Online.jpg'
-            , meta: []
-        }
-        , {
-            id: 1
-            , type: 'nightlife'
-            , name: 'Places 1'
-            , desc: 'This is a Place'
-            , img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Foods_%28cropped%29.jpg/1024px-Foods_%28cropped%29.jpg'
-            , meta: []
-        }
-];
-
-    return {
-
-        getPlaces: function () {
-            return places;
-        }
-    , };
-})
-.factory('businesssearchfactory', function (gconfigfactory, $http, $ionicLoading) {
-
-    return {
-        searchBusinesses: function (keyword) {
-            var lat = 0;
-            var long = 0;
-            var result = null;
-            navigator.geolocation.getCurrentPosition(function (position) {
-                lat = position.coords.latitude;
-                long = position.coords.longitude;
-
-                $ionicLoading.show({
-                    template: 'Loading...'
-                }).then(function () {
-                    console.log("The loading indicator is now displayed");
-                });
-
-                $http({
-                    url: gconfigfactory.getGooglePlacesUrl() + '?location=' + lat + ',' + long + '&radius=500&type=food&key=' + gconfigfactory.getGooglePlacesAPI()
-                    , dataType: 'JSONP'
-                    , method: 'GET'
-                }).success(function (res) {
-                    result = res;
-                    $ionicLoading.hide();
-                });
-            });
-            return result;
-        }
-
-    }
-})
 .factory('ratingsreviewsfactory', function (gconfigfactory, $http, $ionicLoading) {
 
     //  {id:0,meta:'',review:'',rate}
