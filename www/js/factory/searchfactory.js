8cbe7ff15771a0914be950c319e561cb36cde31c
Alvin Reyes <alvinpermelonareyes@gmail.com>
2016-07-03 22:18:55 -0400

    change the search radius by 900

--- www/js/factory/searchfactory.js
+++ www/js/factory/searchfactory.js
@@ -37,7 +37,7 @@ angular.module('zatiqctrl.datafactory')
 
                     var request = {
                         location: currentLoc
-                        , radius: '700'
+                        , radius: '900'
                         , types: types
                     };
                         
