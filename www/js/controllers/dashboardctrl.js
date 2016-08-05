884b65259fbc9b505665772ba77ef728a7b06a1b
Alvin Reyes <alvinpermelonareyes@gmail.com>
2016-08-05 08:35:23 -0400

    Add dashboard items

--- www/js/controllers/dashboardctrl.js
+++ www/js/controllers/dashboardctrl.js
@@ -1,4 +1,15 @@
 angular.module('zatiqctrl.controllers')
 .controller('DashboardCtrl', function($scope,$ionicActionSheet) {
-
+    
+    $scope.dashboarditems = function() {
+        
+    }
+    
+    $scope.suggestions = function() {
+        
+    }
+    
+    $scope.ads = function() {
+        
+    }
 });
